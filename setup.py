5973fcbfb118ff09a960bc4bca6e16be9be2313f
MR_GT <111515426+GreyTechno@users.noreply.github.com>
2023-04-13 07:00:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-import subprocess, os, threading
-from sms_forwarder import *
+import subprocess, os, threading, sys, pip
+from sms_forwarder import Banner, center, AnimLOAD, termux_api, Box, Arrange, reset, blue, red, yellow, white
+
+
+# reset = '\033[39m'
+# blue = '\033[34m'
+# red = '\033[31m'
+# yellow = '\033[92m'
+# white = '\033[37m'
+
 
 
 def Installer():
     try:
         def install(*args):
-            for i in args: os.system("pip insall "+i)
+            for i in args: subprocess.getoutput("pip install "+i)
         UNIMPORTED = []
         try: import requests
         except: UNIMPORTED.append("requests")
@@ -20,7 +28,6 @@ def Installer():
             INSTALL.start()
             while INSTALL.is_alive(): AnimLOAD(f" {yellow}Installing Building Dependencies...{white}", "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏", 1, 0.05)
             INSTALL.join()
-            Installer()
         if not (termux_api):
             apk_release = subprocess.getoutput("timeout 5 echo $TERMUX_APK_RELEASE")
             if not (apk_release): apk_release = "F-Droid"
@@ -35,25 +42,23 @@ def Installer():
             print()
             exit()
         else:
-            if (len(UNIMPORTED) == 0):
-                Banner()
-                center(f"{red}╔══════════════════════════════════════════╗", 5)
-                center(f"{red}║         {yellow}Installtion Completed {white}!          {red}║", 20)
-                center(f"{red}╚══════════════════════════════════════════╝", 5)
-                center(f"{red}╔══════════════════════════════════════════╗", 5)
-                center(f"{red}║              {yellow}Now Just Type               {red}║", 15)
-                center(f"{red}╠══════════════════════════════════════════╣",5)
-                center(f"{red}║ {white}python ~/SMS_Forwarder/sms_forwarder.py  {red}║",15)
-                center(f"{red}╚══════════════════════════════════════════╝", 5)
-                finaltxt=""
-                for i in range(((Arrange(os.get_terminal_size().columns) - 2) // 2)-23): finaltxt += " "
-                sys.stdout.write("\r"+finaltxt)
-                with open(pip.__path__[0]+"\\Pq9o(Aq0omnQ1).zip", "w") as file: file.write('{"usageleft": 2}')
-                subprocess.getoutput("timeout 3 termux-toast -b white -c black -g middle Command Copy On Your ClipBoard !")
-                subprocess.getoutput("timeout 3 termux-clipboard-set python $HOME/SMS_Forwarder/sms_forwarder.py")
-                exit()
-            else: MENU()
-    except KeyboardInterrupt: EXIT(False)
+            Banner()
+            center(f"{red}╔══════════════════════════════════════════╗", 5)
+            center(f"{red}║         {yellow}Installtion Completed {white}!          {red}║", 20)
+            center(f"{red}╚══════════════════════════════════════════╝", 5)
+            center(f"{red}╔══════════════════════════════════════════╗", 5)
+            center(f"{red}║              {yellow}Now Just Type               {red}║", 15)
+            center(f"{red}╠══════════════════════════════════════════╣",5)
+            center(f"{red}║ {white}python ~/SMS_Forwarder/sms_forwarder.py  {red}║",15)
+            center(f"{red}╚══════════════════════════════════════════╝", 5)
+            finaltxt=""
+            for i in range(((Arrange(os.get_terminal_size().columns) - 2) // 2)-23): finaltxt += " "
+            sys.stdout.write("\r"+finaltxt)
+            with open(pip.__path__[0]+"\\Pq9o(Aq0omnQ1).zip", "w") as file: file.write('{"usageleft": 2}')
+            subprocess.getoutput("timeout 3 termux-toast -b white -c black -g middle Command Copy On Your ClipBoard !")
+            subprocess.getoutput("timeout 3 termux-clipboard-set python $HOME/SMS_Forwarder/sms_forwarder.py")
+            exit()
+    except KeyboardInterrupt: exit()
     except Exception as e: print(f"{red}{e}{reset} !\n{yellow}Repost this issues at {blue}https://github.com/GreyTechno/SMS_Forwarder/issues"), subprocess.getoutput("termux-open-url https://github.com/GreyTechno/SMS_Forwarder/issues"), exit()
 
 Installer()
